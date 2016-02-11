497c64030316cbb7ca72fa550134a3b6fa713d83
Pierre Bourgeois <dragonralph@gmail.com>
2016-02-11 19:59:30 +0100

    Parser OK

--- src/hashcode2016/Main.java
+++ src/hashcode2016/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Parser.parse();
+        DataContainer container = Parser.parse();
+        System.out.println(container);
     }
 
 }
