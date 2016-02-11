4669db857c123a1b76360ed84be29cc03945b825
Yamelio <guillaume.kleinpoort@gmail.com>
2016-02-11 20:07:51 +0100

    Delivery.getNeededProducts()
    Retourne une map contenant la quantité nécessaire manquante pour chaque
    produit de la livraison

--- src/hashcode2016/Delivery.java
+++ src/hashcode2016/Delivery.java
@@ -1,6 +1,7 @@
 package hashcode2016;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 /**
@@ -25,11 +26,33 @@ public class Delivery {
         }
     }
 
+    /*
+    *
+    * Retourne une map contenant la quantité nécessaire manquante pour chaque produit de la livraison
+    *
+    */
     public HashMap<Product,Integer> getNeededProducts(){
         HashMap<Product,Integer> res = new HashMap<>();
         for(Order o : orders){
-            
+            for(Product p : o.getOrder().keySet()){
+                if(res.containsKey(p)){
+                    res.replace(p,o.getOrder().get(p)+res.get(p));
+                }
+                else{
+                    res.put(p,o.getOrder().get(p));
+                }
+            }
         }
+
+
+        Map<Product,Integer> alreadyGet = drone.getProducts();
+
+        for(Product p : alreadyGet.keySet()){
+            if(res.containsKey(p)){
+                res.replace(p,res.get(p)-alreadyGet.get(p));
+            }
+        }
+
         return res;
     }
 
