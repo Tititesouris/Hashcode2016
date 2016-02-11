9e9f0996ee3a9c4cce5b1c9ca95132c42d45242a
Yamelio <guillaume.kleinpoort@gmail.com>
2016-02-11 19:03:26 +0100

    Init warehouse

---
+++ src/hashcode2016/Warehouse.java
@@ -0,0 +1,14 @@
+package hashcode2016;
+
+import java.util.Map;
+
+/**
+ * Created by Guillaume on 11/02/2016.
+ */
+public class Warehouse {
+
+    int x;
+    int y;
+    Map<Product,Integer> products;
+
+}
