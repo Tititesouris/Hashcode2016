ba474ce74ef837030ea41b228de0a4dec36fdd12
Pierre Bourgeois <dragonralph@gmail.com>
2016-02-11 21:43:29 +0100

    au cas ou

--- src/hashcode2016/Main_testPierre.java
+++ src/hashcode2016/Main_testPierre.java
@@ -16,12 +16,12 @@ public class Main_testPierre {
         System.out.println("Hello World!");
         container = Parser.parse("mother_of_all_warehouses.in");
         Simulation sim = new Simulation(container);
-        map=sim.getMap();
+        map = sim.getMap();
         System.out.println(container);
 
         drones = sim.getDrones();
         deliveries = new ArrayList<>();
-        orders=sim.getOrders();
+        orders = sim.getOrders();
 
         // get first drones
         // order orders by distance from warehouses
@@ -30,8 +30,27 @@ public class Main_testPierre {
         Warehouse warehouseDrones = warehouses.get(0);
         int x = warehouseDrones.getX(), y = warehouseDrones.getY();
 
-        System.out.println("Init: "+ x +", "+y);
-        
+        System.out.println("Init: " + x + ", " + y);
+        int i = 0, numberDrones=orders.size();
+        // foreach drones
+        for (Drone drone : drones) {
+            // selection d'un client au hasard
+            if(i>numberDrones){
+                break;
+            }
+            Order order = orders.get(i);
+
+            java.util.Map<Product, Integer> needs = order.getOrder();
+          /*  for (:
+                 ) {
+                
+            }
+            // check have enought
+            warehouseDrones.unload()*/
+
+            i++;
+        }
+
 
     }
 
