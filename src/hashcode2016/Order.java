f08a87b4f7bd3d5c4651d624b95b95d909c1bede
MizuMaro <lil_hasni@hotmail.fr>
2016-02-11 19:14:07 +0100

    Creation de l'objet Order avec int id en parametre qui crée une hashmap <produit,quantité>

---
+++ src/hashcode2016/Order.java
@@ -0,0 +1,18 @@
+package hashcode2016;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Order {
+	int id ; 
+	Map<Product,Integer> order;
+	
+	public Order(int id){
+		this.id = id;
+		this.order = new HashMap<Product, Integer>();
+	}
+	
+	void addProduct(Product p, int quantity){
+		
+	}
+}
