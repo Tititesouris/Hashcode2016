09d6486cbbd41fad1dda6729b3fb42f549ea4898
Pierre Bourgeois <dragonralph@gmail.com>
2016-02-11 20:16:26 +0100

    DataOutput

--- src/hashcode2016/Main.java
+++ src/hashcode2016/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        DataContainer container = Parser.parse();
+        DataContainer container = Parser.parse("mother_of_all_warehouses.in");
         System.out.println(container);
     }
 
